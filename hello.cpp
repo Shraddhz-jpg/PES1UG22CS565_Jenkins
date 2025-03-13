914eb77a3ea537273d964bdaae7f4bf8191f840e
Shraddhz-jpg <shraddha.pesu@gmail.com>
2025-03-13 16:41:49 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "Shraddha PES1UG22CS565\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
